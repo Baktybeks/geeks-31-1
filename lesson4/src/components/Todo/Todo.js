358b9cbddf80d1fa4d619dfa6b4cec443b9beae8
Baktybek Sariev <baktybeks@mail.ru>
2023-12-20 21:46:29 +0600

    lesson4

---
+++ lesson4/src/components/Todo/Todo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import classes from './todo.module.css';
+
+
+const Todo = (
+  {
+    task,
+    handleDelete,
+    handleDone,
+    handleSelectCurrent,
+    isEdit
+  }) => {
+  const [input, setInput] = useState(task.title)
+  if (isEdit) {
+    return <div>
+      <input
+        type="text"
+      value={input}
+      onChange={event =>
+        setInput(event.target.value)
+      }/>
+      <button>Сохранить</button>
+      <button>Отмена</button>
+    </div>;
+  }
+  return (
+    <div className={classes.todo}>
+      <h5>{task.id}   {task.title}</h5>
+      <button onClick={()=>{handleSelectCurrent(task.id)}}>Редактировать</button>
+      <button onClick={()=>{handleDone(task.id)}}>Done</button>
+      <button onClick={()=>{handleDelete(task.id)}}>Удалить</button>
+    </div>
+  );
+};
+
+export default Todo;
